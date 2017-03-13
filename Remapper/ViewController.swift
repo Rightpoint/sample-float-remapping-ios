5f8903c390cd14fff85cef082fc863b4c4c9073f
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2017-03-12 22:01:56 -0400

    Build demo.

--- Remapper/ViewController.swift
+++ Remapper/ViewController.swift
@@ -9,12 +9,96 @@
 import UIKit
 import Swiftilities
 
-class ViewController: UIViewController {
+// Here is where the magic happens
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+extension ViewController: UITableViewDelegate {
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateHairline()
     }
 
 }
 
+extension ViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateHairline()
+    }
+
+}
+
+private extension ViewController {
+
+    func updateHairline() {
+        let offset = tableView.contentOffset.y
+        print(offset)
+        hairlineView.alpha = offset.scaled(from: 3...13, to: 0...1)
+    }
+
+}
+
+// Everything after this line is just boring table view setup code
+
+class ViewController: UIViewController {
+
+    @IBOutlet var hairlineView: UIView!
+    @IBOutlet var tableView: UITableView!
+
+    let names = [
+        "Balin",
+        "Bard",
+        "Beorn",
+        "Bert",
+        "Bifur",
+        "Bilbo",
+        "Bofur",
+        "Bolg",
+        "Bombur",
+        "Dain",
+        "Dori",
+        "Dwalin",
+        "The Elvenking",
+        "Fili",
+        "Gandalf the Grey",
+        "Gloin",
+        "Gollum",
+        "The Great Goblin",
+        "Kili",
+        "Nori",
+        "Oin",
+        "Ori",
+        "Smaug",
+        "Thorin Oakenshield",
+        "Tom",
+        "William",
+        ]
+
+}
+
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count + 1 // + 1 is for title cell
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            return tableView.dequeueReusableCell(withIdentifier: "TitleCell", for: indexPath)
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterCell", for: indexPath) as! CharacterCell
+            cell.label.text = names[indexPath.row - 1]
+            return cell
+        }
+    }
+
+}
+
+class TitleCell: UITableViewCell { }
+
+class CharacterCell: UITableViewCell {
+
+    @IBOutlet var label: UILabel!
+
+}
